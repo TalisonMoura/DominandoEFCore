b547f2bd9c57da069748b3db15d7f58d9f0c2722
Talison Moura <talisonmoura13@gmail.com>
2024-12-29 21:36:33 -0300

    carregamento adiantado

---
+++ Modules/ModuleDeepDrive.cs
@@ -0,0 +1,49 @@
+﻿using DominandoEFCore.Data;
+using DominandoEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominandoEFCore.Modules;
+
+public class ModuleDeepDrive
+{
+    public ModuleDeepDrive()
+    {
+        AdvanceLoading();
+    }
+
+    static ApplicationDbContext GetContext() => new();
+
+    static void AdvanceLoading()
+    {
+        using var db = GetContext();
+        SetupLoadingType(db);
+
+        var departments = db.Departments.Include(x => x.Employees);
+        foreach (var department in departments)
+        {
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine($"Department: {department.Description}");
+
+            if (department.Employees?.Count > 0)
+                foreach (var employee in department.Employees)
+                    Console.WriteLine($"\tEmployee: {employee.Name}");
+            else
+                Console.WriteLine("\t No employees were found");
+        }
+    }
+
+    static void SetupLoadingType(ApplicationDbContext context)
+    {
+        if (!context.Departments.Any())
+        {
+            context.Departments.AddRange
+            (
+                new Department { Description = "Department 1", Employees = [new() { Name = "Talison Moura", Cpf = "00000000001", Rg = "MG00000001" }] },
+                new Department { Description = "Department 2", Employees = [new() { Name = "Raphael Moura", Cpf = "00000000011", Rg = "MG00000011" }, new() { Name = "Marcio Moura", Cpf = "00000000111", Rg = "MG00000111" }] }
+            );
+
+            context.SaveChanges();
+            context.ChangeTracker.Clear();
+        }
+    }
+}
---
+++ Modules/ModuleEfDatabase.cs
@@ -0,0 +1,161 @@
+﻿using System.Diagnostics;
+using DominandoEFCore.Data;
+using DominandoEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DominandoEFCore.Modules;
+
+public class ModuleEfDatabase
+{
+    private static int Count { get; set; }
+
+    public ModuleEfDatabase()
+    {
+
+        //EnsureCreatedAndDeleted();
+        //GapEnsureCreated();
+        //HealthCheckOnDataBse();
+
+        //warmup
+        //GetContext().Departments.AsNoTracking().Any();
+        //Count = 0;
+        //HandleConnectionState(false);
+        //Count = 0;
+        //HandleConnectionState(true);
+
+        //ExecuteSQL();
+        //SqlInjection();
+        //PendingMigrates();
+        //ExecutePendindMigrationsOnExecutionTime();
+        //GetAllMigrates();
+        //GetAppliedMigrates();
+        GeneralDatabaseScritp();
+    }
+
+    static ApplicationDbContext GetContext() => new();
+
+    static void EnsureCreatedAndDeleted()
+    {
+        using var db = GetContext();
+        db.Database.EnsureCreated();
+        db.Database.EnsureDeleted();
+    }
+
+    static void GapEnsureCreated()
+    {
+        using var dbDefault = GetContext();
+        using var dbCity = new ApplicationDbContextCity();
+
+        dbDefault.Database.EnsureCreated();
+        dbCity.Database.EnsureCreated();
+
+        var databaseCreator = dbCity.GetService<IRelationalDatabaseCreator>();
+        databaseCreator.CreateTables();
+    }
+
+    static void HealthCheckOnDataBse()
+    {
+        using var db = GetContext();
+        var canConnect = db.Database.CanConnect();
+
+        if (canConnect)
+            Console.WriteLine("I'm able to connect");
+        else
+            Console.WriteLine("I'm not able to connect");
+    }
+
+    static void HandleConnectionState(bool isHandleConnectionState)
+    {
+        using var db = GetContext();
+        var time = Stopwatch.StartNew();
+
+        var connection = db.Database.GetDbConnection();
+
+        connection.StateChange += (_, _) => ++Count;
+
+        if (isHandleConnectionState)
+            connection.Open();
+
+        for (int i = 0; i < 200; i++)
+            db.Departments.AsNoTracking().Any();
+
+        time.Stop();
+        Console.WriteLine($"TotalTime: {time.Elapsed}, {isHandleConnectionState}, {Count}");
+    }
+
+    static void ExecuteSQL()
+    {
+        using var db = GetContext();
+        using var cmd = db.Database.GetDbConnection().CreateCommand();
+        cmd.CommandText = "SELECT 1";
+        var rowsaffected = cmd.ExecuteNonQuery();
+
+        Console.WriteLine($"Number of rows affected: {rowsaffected}");
+
+        string rawCityName = "BH É NOISS";
+        Guid cityId = Guid.Parse("55D7CF2C-A4A3-4D16-9B0E-55519CBFD57A");
+        var rowsaffectedraw = db.Database.ExecuteSqlRaw("update Cities set Name={0} where Id={1}", rawCityName, cityId);
+        Console.WriteLine($"Number of rows affected sqlraw: {rowsaffectedraw}");
+
+        string interpolatedCityName = "BH É NOISS <3 PRAÇA SETE, FOTO NA HORA É FOTO";
+        var rowsaffectedinterpolated = db.Database.ExecuteSqlInterpolated($"update Cities set Name={interpolatedCityName} where Id={cityId}");
+        Console.WriteLine($"Number of rows affected sqlinterpolated: {rowsaffectedinterpolated}");
+    }
+
+    static void SqlInjection()
+    {
+        using var db = GetContext();
+        db.Database.EnsureDeleted();
+        GapEnsureCreated();
+
+        db.Departments.AddRange
+        (
+            new Department { Description = "Department 1" },
+            new Department { Description = "Department 2" }
+        );
+
+        db.SaveChanges();
+
+        string description = "Teste ' or 1=' 1";
+        var rowsaffectedraw = db.Database.ExecuteSqlRaw($"update Departments set Description='AttackSqlIjection' where Description='{description}'");
+
+        foreach (var department in db.Departments.AsNoTracking())
+            Console.WriteLine($"ID: {department.Id}, DESCRIPTION: {department.Description}");
+    }
+
+    static void PendingMigrates()
+    {
+        using var db = GetContext();
+        var migratesPending = db.Database.GetPendingMigrations();
+        Console.WriteLine($"Migrates pending:\n{string.Join("\n", migratesPending)}, Total Migrates: {migratesPending.Count()}");
+    }
+
+    static void ExecutePendindMigrationsOnExecutionTime()
+    {
+        using var db = GetContext();
+        db.Database.Migrate();
+    }
+
+    static void GetAllMigrates()
+    {
+        using var db = GetContext();
+        var migrates = db.Database.GetMigrations();
+        Console.WriteLine($"Migrates:\n{string.Join("\n", migrates)}, Total Migrates: {migrates.Count()}");
+    }
+
+    static void GetAppliedMigrates()
+    {
+        using var db = GetContext();
+        var migrates = db.Database.GetAppliedMigrations();
+        Console.WriteLine($"Migrates:\n{string.Join("\n", migrates)}, Total Migrates: {migrates.Count()}");
+    }
+
+    static void GeneralDatabaseScritp()
+    {
+        using var db = GetContext();
+        var script = db.Database.GenerateCreateScript();
+        Console.WriteLine(script);
+    }
+}
